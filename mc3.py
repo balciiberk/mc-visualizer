1b3773da2f6ce0188d068e6cb03b4cd8919b8aa5
berk <balciiberk@gmail.com>
2021-06-03 17:00:21 +0300

    added the first code

---
+++ mc3.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import os
+
+
+def init():
+    global inside, outside, a, b, c, pi_list
+    a=[-0.5,0.5]
+    b=[-0.5,-0.5]
+    c=[0.5,0.5]
+    ax.plot(a,b,'-r',a,c, '-r',b,a, '-r',c,a, '-r',linewidth=1)
+    ax.add_patch(plt.Circle((0,0),0.5,color='g',fill=False))
+    inside=[]
+    outside=[]
+    pi_list = []
+def repeat_f(i):
+    if i % 2 == 0:
+        x,y = np.random.rand(2)-.5
+        if np.sqrt(x**2+y**2)<.5:
+            inside.append([x,y])
+        else:
+            outside.append([x,y])
+        ax.scatter(x,y,c='b')
+        pi_current = 4*len(inside)/(len(inside)+len(outside))
+        pi_list.append(pi_current)
+        print(f'Pi = {pi_current:.4f}')
+    else:
+        ax.clear()
+        ax.plot(a,b,'-r',a,c, '-r',b,a, '-r',c,a, '-r',linewidth=1)
+        ax.add_patch(plt.Circle((0,0),0.5,color='g',fill=False))
+        if inside != []:
+            ax.scatter(*zip(*inside),c='g')
+        if outside != []:
+            ax.scatter(*zip(*outside),c='r')
+def graph(pi_list):
+    plt.plot(range(len(pi_list)), pi_list,'-b')
+    plt.plot([0,len(pi_list)],[np.pi,np.pi],'--r')
+    plt.show()
+if __name__=="__main__":
+    #clear()
+    inter=1
+    fig, ax =plt.subplots(figsize=(8, 8), dpi=80)
+    ani = FuncAnimation(fig, repeat_f, init_func=init, frames=1000,repeat=False, interval=inter)
+    plt.show()
+    graph(pi_list)
